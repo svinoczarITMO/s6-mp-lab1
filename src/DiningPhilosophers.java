b5e518d2cecfee2af3e7b0f9ab5b17a35e6a4c12
svinoczar <alexsupper75@gmail.com>
2025-04-16 14:07:36 +0300

    refactor: code

--- src/DiningPhilosophers.java
+++ src/DiningPhilosophers.java
@@ -1,51 +1,44 @@
-import java.util.Arrays;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class DiningPhilosophers {
-
+    private final Lock[] forks;
+    private static final Lock globalLock = new ReentrantLock(); 
+    
     public DiningPhilosophers() {
-        forks = new ReentrantLock[FORKS_COUNT];
-        Arrays.fill(forks, new ReentrantLock());
+        forks = new ReentrantLock[5];
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new ReentrantLock();
+        }
     }
 
-    private ReentrantLock[] forks;
-    private int PHILOSOPHERS_COUNT = 5;
-    private int FORKS_COUNT = PHILOSOPHERS_COUNT;
-
-    // call the run() method of any runnable to execute its code
     public void wantsToEat(int philosopher,
-            Runnable pickLeftFork,
-            Runnable pickRightFork,
-            Runnable eat,
-            Runnable putLeftFork,
-            Runnable putRightFork) throws InterruptedException {
+                         Runnable pickLeftFork,
+                         Runnable pickRightFork,
+                         Runnable eat,
+                         Runnable putLeftFork,
+                         Runnable putRightFork) throws InterruptedException {
+        
+        globalLock.lock();
+        try {
+            int leftFork = philosopher;
+            int rightFork = (philosopher + 1) % 5;
 
-        Runnable pickFirstFork, pickSecondFork, putFirstFork, putSecondFork;
-        int rightFork = philosopher;
-        int leftFork = (philosopher + 1) % PHILOSOPHERS_COUNT;
-        int firstFork = Math.min(rightFork, leftFork);
-        int secondFork = Math.max(firstFork, leftFork);
+            forks[leftFork].lock();
+            pickLeftFork.run();
+            
+            forks[rightFork].lock();
+            pickRightFork.run();
 
-        if (firstFork == rightFork) {
-            pickFirstFork = pickRightFork;
-            pickSecondFork = pickLeftFork;
-            putFirstFork = putRightFork;
-            putSecondFork = putLeftFork;
-        } else {
-            pickFirstFork = pickLeftFork;
-            pickSecondFork = pickRightFork;
-            putFirstFork = putLeftFork;
-            putSecondFork = putRightFork;
+            eat.run();
+
+            putRightFork.run();
+            forks[rightFork].unlock();
+            
+            putLeftFork.run();
+            forks[leftFork].unlock();
+        } finally {
+            globalLock.unlock();
         }
-
-        forks[firstFork].lock();
-        pickFirstFork.run();
-        forks[secondFork].lock();
-        pickSecondFork.run();
-        eat.run();
-        putSecondFork.run();
-        forks[secondFork].unlock();
-        putFirstFork.run();
-        forks[firstFork].unlock();
     }
 }
